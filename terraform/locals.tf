082eeae141541249700b703e0f8faf72cec7e66b
seolman <tjfehdgns@gmail.com>
2026-07-08 16:53:38 +0900

    add: 2 vm for k3s

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,6 @@
 locals {
   # oci_rocky_linux_9_aarch_source_id = "ocid1.image.oc1..aaaaaaaas7a4zwwsdtry2nsf6rqrvhgasczcyb2wxsx6x3pewxorcwr3d4pq"
+  oci_ubuntu_24_04_aarch_source_id = "ocid1.image.oc1.ap-seoul-1.aaaaaaaae5dqssutnmejddxsq54jjg325skera6l57vgsohths75dem3sp4a"
   oci_instance_shape = "VM.Standard.A1.Flex"
 
   common_tags = {
