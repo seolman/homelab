14e6d6710fa8cc25d5b91e4a86638579b4312c43
seolman <tjfehdgns@gmail.com>
2026-07-08 16:57:14 +0900

    add: todo

--- terraform/main.tf
+++ terraform/main.tf
@@ -237,6 +237,7 @@ resource "oci_core_instance" "homelab_dev_vm_1" {
   }
   metadata = {
     ssh_authorized_keys = var.my_public_key
+    # TODO cloud-init
   }
   freeform_tags = local.common_tags
 }
@@ -262,6 +263,7 @@ resource "oci_core_instance" "homelab_dev_vm_2" {
   }
   metadata = {
     ssh_authorized_keys = var.my_public_key
+    # TODO cloud-init
   }
   freeform_tags = local.common_tags
 }
